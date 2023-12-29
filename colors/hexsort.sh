a98ffb186ab8e2cb1c6741f4346c4ef8adf067e5
LeonardParker5 <leonard.parker5@gmail.com>
2023-12-29 10:59:19 -0500

    Color data and hex sorter

---
+++ colors/hexsort.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+input_file="colors.txt"
+
+temp_file=$(mktemp)
+
+# Convert hex values to decimal and append to temporary file
+awk '{ printf "%d %s\n", "0x" $1, $0 }' "$input_file" > "$temp_file"
+
+# Sort the temporary file based on the first column (decimal values)
+sort -n -k1 -o "$temp_file" "$temp_file"
+
+# Print the sorted results with original hex values and color names
+cat "$temp_file" | cut -d' ' -f2-
+
+rm "$temp_file"
+
